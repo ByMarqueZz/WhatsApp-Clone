9b6115d751dbb049217446d6b535ed500259a195
Juan Antonio Marquez Ruiz <115186741+ByMarqueZz@users.noreply.github.com>
2023-02-22 12:50:47 +0100

    a

---
+++ servidor/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+var users = 0;
+var usuariosConectados = [];
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+    users++;
+    console.log('a user connected, hay ' + users + ' usuarios conectados');
+    socket.on('disconnect', (socket) => {
+        users--;
+        console.log('a user disconnected, hay ' + users + ' usuarios conectados');
+    });
+});
+
+server.listen(3001, () => {
+  console.log('listening on *:3001');
+});
