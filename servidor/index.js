9d4308916b6ebfd8b0009c3c0d949a822345dca5
Juan Antonio Marquez Ruiz <bymarquezz2@gmail.com>
2023-02-28 00:44:02 +0100

    asdads

--- servidor/index.js
+++ servidor/index.js
@@ -5,21 +5,16 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-var users = 0;
-var usuariosConectados = [];
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', (socket) => {
-    users++;
-    console.log('a user connected, hay ' + users + ' usuarios conectados');
-    socket.on('disconnect', (socket) => {
-        users--;
-        console.log('a user disconnected, hay ' + users + ' usuarios conectados');
-    });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 });
 
-server.listen(3001, () => {
-  console.log('listening on *:3001');
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
