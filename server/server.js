e10ecb70ba11bc21bdcf9b77576b8665acc18fd1
Juan Antonio Marquez Ruiz <bymarquezz2@gmail.com>
2023-03-05 22:10:58 +0100

    fin

--- server/server.js
+++ server/server.js
@@ -5,18 +5,24 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
+app.use(express.static('server/public'));
+
+var users = [];
+var colors = ['red', 'blue', 'green', 'orange', 'purple', 'pink', 'brown', 'lavender', 'lightblue', 'skyblue', 'peachpuff', 'mintcream', 'coral', 'lemonchiffon', 'pink', 'beige', 'ivory', 'lawngreen', 'pink', 'orange', 'blanchedalmond', 'plum', 'turquoise', 'olivedrab', 'gold', 'salmon', 'powderblue', 'rosybrown', 'khaki', 'palegreen', 'tan', 'pink', 'lightgoldenrodyellow', 'palegreen', 'paleturquoise', 'sandybrown', 'mintcream', 'pink', 'papayawhip', 'peachpuff', 'peru', 'lightpink', 'mediumaquamarine', 'lightcyan', 'burlywood', 'wheat', 'palevioletred', 'thistle', 'cornsilk', 'blanchedalmond', 'bisque', 'moccasin', 'mistyrose', 'lavenderblush', 'seashell'];
 
 io.on('connection', (socket) => {
-  io.emit('userConnected', 'User connected');
+
+  socket.on('userLogin', (user) => {
+    users.push({user: user.user, color: colors[Math.floor(Math.random() * colors.length)], image: user.image});
+    io.emit('userLogged', users);
+    io.emit('userConnected', user.user)
+  });
 
   socket.on('disconnect', () => {
-    io.emit('userDisconnected', 'User disconnected');
+    io.emit('userDisconnected', 'User');
   });
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  socket.on('chat message', (json) => {
+    io.emit('chat message', {json: json, color: users.find(x => x.user === json.user).color});
   });
 });
 
